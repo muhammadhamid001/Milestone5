567b318fa00077ff93c8560b97f857a4a4d17598
Muhammad Hamid <hamidrayyan72@gmail.com>
2024-11-10 22:36:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+"use strict";
+// Get references to the form and display area
+var form = document.getElementById('resume-form');
+var resumeDisplayElement = document.getElementById('resume-display');
+// Handle fomr submission
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); // prevent page reload
+    // collect input values
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const education = document.getElementById('education').value;
+    const experience = document.getElementById('experience').value;
+    const skills = document.getElementById('skills').value;
+    //Generate the resume content dynamically
+    const resumeHTML = `
+    <h2><b>Editable Resume</b></h2>
+    <h3>Personal Information</h3>
+    <p><b>Name:</b><span contenteditable="true">${name}</span></p>
+    <p><b>Email:</b><span contenteditable="true">${email}</span></p>
+    <p><b>Phone:</b><span contenteditable="true">${phone}</span></p>
+
+    <h3>Education</h3>
+    <p contenteditable="true">>${education}</P>
+
+    <h3>Experience</h3>
+    <p contenteditable="true">>${experience}</p>
+    
+    <h3>Skills</h3>
+    <p contenteditable="true">>${skills}</P>
+    `;
+    // Display the generated resume
+    if (resumeDisplayElement) {
+        resumeDisplayElement.innerHTML = resumeHTML;
+    }
+    else {
+        console.error(`the resume display element is missing.`);
+    }
+});
